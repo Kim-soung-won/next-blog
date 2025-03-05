35f61d0dc9093e544d33bb3007749bbae63610ac
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-05 18:03:26 +0900

    nextjs 캐시비우기 추가

--- src/shared/db/meals/actions.ts
+++ src/shared/db/meals/actions.ts
@@ -3,12 +3,14 @@
 import { Meal } from "@/entities/meals";
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function shareMeal(meal: Meal, image: File) {
-  'use server';
-
   const mealDto: Meal = meal;
 
   await saveMeal(mealDto, image);
+
+  // 해당 경로의 캐시 새로고침, layout : 하위 페이지들도 함께 캐싱 새로고침
+  revalidatePath("/meals", "layout");
   redirect("/meals");
 }
