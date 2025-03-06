f2a9e37432e0fb5e7c058797da4501bce12bc00f
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-06 09:42:39 +0900

    페이지 동적, 정적 Metadata 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import "./globals.css";
 import MainHeader from "@/features/header/main-header";
 
+/**
+ * Nextjs는 모든 page, layout에 지정된 metadata를 수집하여 사용자에게 보여줍니다.
+ * 이 metadata는 SEO(Search Engine Optimization)에 사용된다.
+ * title: 페이지의 제목
+ * description: 페이지의 설명
+ * 등등 https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+ * 
+ */
 export const metadata = {
   title: "NextLevel Food",
   description: "Delicious meals, shared by a food-loving community.",
--- app/meals/[id]/page.tsx
+++ app/meals/[id]/page.tsx
@@ -1,13 +1,36 @@
-import Link from "next/link";
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/shared/db/meals/meals";
 import { notFound } from "next/navigation";
 
+
+
 // 폴더 명[] 안의 id를 받아올 수 있다.
 interface Params {
   id: string;
 }
+
+/**
+ * 동적 MetaData 생성
+ * Nextjs는 모든 page, layout에 지정된 metadata를 수집하여 사용자에게 보여줍니다.
+ * 이 metadata는 SEO(Search Engine Optimization)에 사용된다.
+ * title: 페이지의 제목
+ * description: 페이지의 설명
+ * 등등 https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+ * 
+ */
+export async function generateMetadata({ params }: { params: Params }) {
+  const meal = await getMeal(parseInt(params.id));
+
+  if(!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.instructions,
+  };
+};
 
 export default async function MealDetailsPage({ params }: { params: Params }) {
   const meal = await getMeal(parseInt(params.id));
