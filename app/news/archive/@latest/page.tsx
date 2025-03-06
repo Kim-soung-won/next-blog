06573150ac9894d435eab7e595333b09b0f4a64a
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-06 11:28:30 +0900

    병렬 라우트 페이지 추가

---
+++ app/news/archive/@latest/page.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function LatestNewsPage(): ReactNode {
+  return <h1>LatestNews Page</h1>;
+}
---
+++ app/news/archive/page.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function ArchivePage(): ReactNode {
+  return <div></div>;
+}
