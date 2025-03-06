e68c56d7873af4a465cdc3f0033c9e23ee1adf65
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-06 15:30:46 +0900

    복수 Path 운용 추가

--- app/news/archive/@archive/[[...filter]]/page.tsx
+++ app/news/archive/@archive/[[...filter]]/page.tsx
@@ -1,7 +1,9 @@
 import NewsList from "@/features/news/main/news-list";
 import {
+  getAvailableNewsMonths,
   getAvailableNewsYears,
   getNewsForYear,
+  getNewsForYearAndMonth,
 } from "@/shared/dummy-data/news";
 import Link from "next/link";
 
@@ -30,9 +32,16 @@ export default async function FilterNewsPage({
   const selectedMonth = filter?.[1];
 
   let news;
+  let links = getAvailableNewsYears();
 
   if (selectedYear && !selectedMonth) {
     news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
   }
 
   let newsContent = <p>No news found for the selected</p>;
@@ -41,17 +50,21 @@ export default async function FilterNewsPage({
     newsContent = <NewsList news={news} />;
   }
 
-  const links = getAvailableNewsYears();
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
-              <li key={link}>
-                <Link href={`/news/archive/${link}`}>{link}</Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/news/archive/${selectedYear}/${link}`
+                : `/news/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
