c20de120ba5ba0acf05246d4cd25b5332f8938d1
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-07 14:25:07 +0900

    RSC DataFetch

--- app/news/archive/@latest/default.tsx
+++ app/news/archive/@latest/default.tsx
@@ -1,39 +1,14 @@
-"use client";
 import NewsList from "@/features/news/main/news-list";
-import { getLatestNews } from "@/shared/dummy-data/news";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 
-export default function LatestNewsPage(): ReactNode {
-  const latestNews = getLatestNews();
+export default async function LatestNewsPage(): Promise<ReactNode> {
+  const response = await fetch("http://localhost:8080/news");
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>(null);
-  const [news, setNews] = useState([]);
-
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("서버 오류");
-        throw new Error("서버 오류");
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>로딩 중...</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+  const news = await response.json();
 
   return (
     <>
