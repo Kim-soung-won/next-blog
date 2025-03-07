8f231696091aab8f36e8765789c1df938de28fec
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-03-07 11:04:11 +0900

    _backend 추가

---
+++ _backend/app.ts
@@ -0,0 +1,137 @@
+import express from 'express';
+import pkg from 'pg';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+const DUMMY_NEWS = [
+  {
+    id: 'n1',
+    slug: 'will-ai-replace-humans',
+    title: 'Will AI Replace Humans?',
+    image: 'ai-robot.jpg',
+    date: '2021-07-01',
+    content:
+      'Since late 2022 AI is on the rise and therefore many people worry whether AI will replace humans. The answer is not that simple. AI is a tool that can be used to automate tasks, but it can also be used to augment human capabilities. The future is not set in stone, but it is clear that AI will play a big role in the future. The question is how we will use it.',
+  },
+  {
+    id: 'n2',
+    slug: 'beaver-plague',
+    title: 'A Plague of Beavers',
+    image: 'beaver.jpg',
+    date: '2022-05-01',
+    content:
+      'Beavers are taking over the world. They are building dams everywhere and flooding entire cities. What can we do to stop them?',
+  },
+  {
+    id: 'n3',
+    slug: 'couple-cooking',
+    title: 'Spend more time together!',
+    image: 'couple-cooking.jpg',
+    date: '2024-03-01',
+    content:
+      'Cooking together is a great way to spend more time with your partner. It is fun and you get to eat something delicious afterwards. What are you waiting for? Get cooking!',
+  },
+  {
+    id: 'n4',
+    slug: 'hiking',
+    title: 'Hiking is the best!',
+    image: 'hiking.jpg',
+    date: '2024-01-01',
+    content:
+      'Hiking is a great way to get some exercise and enjoy the great outdoors. It is also a great way to clear your mind and reduce stress. So what are you waiting for? Get out there and start hiking!',
+  },
+  {
+    id: 'n5',
+    slug: 'landscape',
+    title: 'The beauty of landscape',
+    image: 'landscape.jpg',
+    date: '2022-07-01',
+    content:
+      'Landscape photography is a great way to capture the beauty of nature. It is also a great way to get outside and enjoy the great outdoors. So what are you waiting for? Get out there and start taking some pictures!',
+  },
+];
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+});
+
+// 데이터베이스 초기화 함수
+async function initDb() {
+  const client = await pool.connect();
+
+  await client.query(`SET search_path TO ${process.env.DB_SCHEMA}`);
+
+  try {
+    // 테이블 생성
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS news (
+        id SERIAL PRIMARY KEY,
+        slug TEXT UNIQUE,
+        title TEXT,
+        content TEXT,
+        date TEXT,
+        image TEXT
+      )
+    `);
+
+    // 기존 데이터 확인
+    const result = await client.query('SELECT COUNT(*) AS count FROM news');
+    const count = parseInt(result.rows[0].count);
+
+    // 데이터가 없을 경우 삽입
+    if (count === 0) {
+      const insertQuery = `
+        INSERT INTO news (slug, title, content, date, image)
+        VALUES ($1, $2, $3, $4, $5)
+      `;
+
+      for (const news of DUMMY_NEWS) {
+        await client.query(insertQuery, [
+          news.slug,
+          news.title,
+          news.content,
+          news.date,
+          news.image,
+        ]);
+      }
+      console.log('DUMMY_NEWS 데이터를 삽입했습니다.');
+    }
+  } catch (error) {
+    console.error('데이터베이스 초기화 중 오류 발생:', error);
+  } finally {
+    client.release();
+  }
+}
+
+const app = express();
+
+app.use(cors())
+
+app.get('/news', async (req, res) => {
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM news');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('뉴스 데이터 조회 중 오류 발생:', error);
+    res.status(500).json({ error: '서버 오류' });
+  } finally {
+    client.release();
+  }
+});
+
+
+initDb().then(() => {
+  app.listen(8080, () => {
+    console.log('서버가 http://localhost:8080 에서 실행 중입니다.');
+  });
+});
---
+++ _backend/eslint.config.js
@@ -0,0 +1,284 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { fixupPluginRules, fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import _import from "eslint-plugin-import";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import react from "eslint-plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "**/node_modules",
+      "**/dist",
+      "**/.eslintrc.cjs",
+      "**/vite-env.d.ts",
+      "**/vite.config.ts",
+      "**/*.d.ts",
+      "**/*.json",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends("plugin:prettier/recommended", "prettier"),
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      react: react,
+      "react-refresh": reactRefresh,
+      import: fixupPluginRules(_import),
+      "jsx-a11y": jsxA11y,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    rules: {
+      // HTML 사용 중 특수문자 허용
+      "react/no-unescaped-entities": "off",
+      // 기본 React import 비활성화
+      "react/react-in-jsx-scope": "off",
+      // 스프레드 연산자 통제 비활성화
+      "react/jsx-props-no-spreading": "off",
+      // 함수형 컴포넌트 스타일 강제 비활성화
+      "react/function-component-definition": "off",
+      // export 한개만 있을 때 default export 강제하는거 비활성화
+      "import/prefer-default-export": "off",
+      // this 사용 강제 비활성화
+      "class-methods-use-this": "off",
+      // 한 파일에 여러 클래스 정의 불가 비활성화
+      "max-classes-per-file": "off",
+      // 식별자에 _ 사용 금지 비활성화
+      "no-underscore-dangle": "off",
+      // 구조 분해 할당 강제하는거 비활성화
+      "react/destructuring-assignment": ["off"],
+      // throw 키워드 리터럴 값 던지는거 허용
+      "no-throw-literal": "off",
+      // 함수 내에서 반환 여부 일관되지 않아도 허용
+      "consistent-return": "off",
+
+      // React 관련 규칙 강화
+      "react/no-danger": "error",
+      "react/jsx-no-target-blank": ["error", { enforceDynamicLinks: "always" }],
+      "react/jsx-no-script-url": "error",
+      "react/jsx-no-constructed-context-values": "warn",
+
+      // Accessibility 관련 규칙
+      "jsx-a11y/alt-text": "error",
+      "jsx-a11y/anchor-is-valid": [
+        "error",
+        {
+          aspects: ["invalidHref", "preferButton"],
+        },
+      ],
+
+      // TypeScript 관련 규칙 강화
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "": "never",
+          tsx: "never",
+          ts: "never",
+        },
+      ],
+
+      "import/order": [
+        "error",
+        {
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+            },
+            {
+              pattern: "vite",
+              group: "builtin",
+            },
+            {
+              pattern: "@/app/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/shared/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/entities/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/features/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/widgets/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/pages/**",
+              group: "internal",
+              position: "before",
+            },
+
+            {
+              pattern: "@/assest/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "never",
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  ...fixupConfigRules(
+    compat.extends("plugin:eslint-plugin-import/typescript", "prettier"),
+  ).map((config) => ({
+    ...config,
+
+    files: [
+      "**/*.ts",
+      "**/.*.ts",
+      "./**/*.ts",
+      "./**/.*.ts",
+      "**/*.tsx",
+      "**/.*.tsx",
+      "./**/*.tsx",
+      "./**/.*.tsx",
+    ],
+  })),
+  {
+    files: [
+      "**/*.ts",
+      "**/.*.ts",
+      "./**/*.ts",
+      "./**/.*.ts",
+      "**/*.tsx",
+      "**/.*.tsx",
+      "./**/*.tsx",
+      "./**/.*.tsx",
+    ],
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      "react/no-array-index-key": "off",
+      "no-nested-ternary": "off",
+      "react/require-default-props": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-throw-literal": "off",
+
+      "react/jsx-max-props-per-line": [
+        "error",
+        {
+          maximum: 1,
+          when: "always",
+        },
+      ],
+
+      // 들여쓰기 깊이 제한
+      "max-depth": ["error", 2],
+      // 함수의 매개변수 개수 제한
+      "max-params": ["error", 3],
+      // 함수의 길이 제한
+      "max-lines-per-function": ["error", { max: 10 }],
+
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "PascalCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          modifiers: ["destructured"],
+          format: ["camelCase", "PascalCase", "snake_case", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+        },
+      ],
+
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "": "never",
+          tsx: "never",
+          ts: "never",
+        },
+      ],
+
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: [
+            "**/__tests__/**",
+            "**/__mocks__/**",
+            "**/*.test.ts",
+            "**/*.test.tsx",
+            "**/*.spec.ts",
+            "src/shared/lib/test/setup.ts",
+            "src/shared/lib/test/test.lib.tsx",
+          ],
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+];
